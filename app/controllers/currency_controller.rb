685d443f5d1469311f719eff85b0e2d070a01d19
Noah Katz <noahkatz@chicagobooth.edu>
2023-04-18 21:25:39 +0000

    done!

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -20,4 +20,19 @@ class CurrencyController < ApplicationController
 
     render({:template => "currency_templates/step_two.html.erb"})
   end
+  def convert_currency
+    @rawdata = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@rawdata)
+    symbolshash = @parsed_data.fetch("symbols")
+    @array_of_symbols = symbolshash.keys
+    
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+
+    @rawexchange = open("https://api.exchangerate.host/convert?from=" + @from_symbol + "&to=" + @to_symbol).read
+    @parsed_exchange = JSON.parse(@rawexchange)
+    @rate = @parsed_exchange.fetch("result")
+
+    render({:template => "currency_templates/step_three.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get("/forex", {:controller => "currency", :action => "first_currency"})
   get("/forex/:from_currency", {:controller => "currency", :action => "other_currency"})
+  get("/forex/:from_currency/:to_currency", {:controller => "currency", :action => "convert_currency"})
 end
